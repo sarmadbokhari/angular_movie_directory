451ef62905048e7ff9acc09c2ce17bbf51782d1f
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-06-18 17:55:34 -0700

    Set movieOrder to title by default

--- js/controllers.js
+++ js/controllers.js
@@ -3,5 +3,6 @@ var myNameSpace = angular.module('myApp', []);
 myNameSpace.controller('MyController', ['$scope', '$http', function($scope, $http){
   $http.get('js/data.json').success(function(data){
     $scope.movies = data;
+    $scope.movieOrder = "title";
   });
 }]);
