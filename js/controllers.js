55fcfd8d010b6aa940c69d273a4657a638816287
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-06-18 18:32:17 -0700

    Add prevItem nextItem scroll through functionality

--- js/controllers.js
+++ js/controllers.js
@@ -11,5 +11,18 @@ movieControllers.controller('DetailsController', ['$scope', '$http', '$routePara
   $http.get('js/data.json').success(function(data){
     $scope.movies = data;
     $scope.whichItem = $routeParams.itemId;
+
+    if ($routeParams.itemId > 0){
+      $scope.prevItem = Number($routeParams.itemId) - 1;
+    } else {
+      $scope.prevItem = $scope.movies.length-1;
+    }
+
+    if ($routeParams.itemId < $scope.movies.length-1){
+      $scope.nextItem = Number($routeParams.itemId) + 1;
+    } else {
+      $scope.nextItem = 0;
+    }
+
   });
 }]);
